c6e410e8ac47c7fbc9b7b40be7d691f5f2144c3b
ee25btech11006 <ee25btech11006@iith.ac.in>
2025-10-31 21:39:57 +0530

    ee25btech11006

--- ee25btech11006/matgeo/matgeo7/codes/call_c.py
+++ ee25btech11006/matgeo/matgeo7/codes/call_c.py
@@ -1,21 +1,11 @@
 import subprocess
-import os
 
-# Name of the compiled executable
-executable_name = 'lambda_program'
-if os.name == 'nt':  # For Windows
-    executable_name += '.exe'
-
-# Path to the executable (current directory)
-executable_path = os.path.join('.', executable_name)
-
-# Run the C program
+# Run the compiled C program
 result = subprocess.run(
-    [executable_path],
+    ['./code'],   # executable created from code.c
     capture_output=True,
     text=True,
     check=True
 )
 
-# Print the captured output
 print(result.stdout)
