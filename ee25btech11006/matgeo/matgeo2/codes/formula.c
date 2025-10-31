7c79c1eb45788fe91aaf162e69e0e83ceec53e32
ee25btech11006 <ee25btech11006@iith.ac.in>
2025-10-31 19:01:23 +0530

    Refactor formula.c for clarity and modularity

--- ee25btech11006/matgeo/matgeo2/codes/formula.c
+++ ee25btech11006/matgeo/matgeo2/codes/formula.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 #include <math.h>
 
+// Function to calculate discriminant
+int getDiscriminant(int a, int b, int c) {
+    return b * b - 4 * a * c;
+}
+
+// Function to find roots of quadratic equation
+void findRoots(int a, int b, int discrim, int *y1, int *y2) {
+    *y1 = (-b + (int)sqrt(discrim)) / (2 * a);
+    *y2 = (-b - (int)sqrt(discrim)) / (2 * a);
+}
+
+// Function to calculate distance between two points
+double getDistance(int x1, int y1, int x2, int y2) {
+    return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+}
+
 int main() {
     int Ax = 2, Ay = -4;
     int Px = 3, Py = 8;
     int Qx = -10;
 
-     // Quadratic from ||A-P||^2 = ||A-Q||^2
-     // (Ax - Px)^2 + (Ay - Py)^2 = (Ax - Qx)^2 + (Ay - y)^2
+    // From equation ||A-P||^2 = ||A-Q||^2
     int a = 1, b = 8, c = 15;
-    int discrim = b*b - 4*a*c;
+    int discrim = getDiscriminant(a, b, c);
 
-    int y1 = (-b + (int)sqrt(discrim)) / (2*a);
-    int y2 = (-b - (int)sqrt(discrim)) / (2*a);
+    int y1, y2;
+    findRoots(a, b, discrim, &y1, &y2);
 
-    // Distance ||P-Q|| for each y
-    double d1 = sqrt((Px - Qx)*(Px - Qx) + (Py - y1)*(Py - y1));
-    double d2 = sqrt((Px - Qx)*(Px - Qx) + (Py - y2)*(Py - y2));
+    // Calculate distances
+    double d1 = getDistance(Px, Py, Qx, y1);
+    double d2 = getDistance(Px, Py, Qx, y2);
+
+    printf("Roots: y1 = %d, y2 = %d\n", y1, y2);
+    printf("Distances: d1 = %.2f, d2 = %.2f\n", d1, d2);
 
     return 0;
 }
